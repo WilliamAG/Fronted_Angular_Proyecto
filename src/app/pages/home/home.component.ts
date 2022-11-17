2632d9dac47e750a361263cf5a6173c165d3b9d4
luisescamilla999 <luigiernest999@gmail.com>
2022-11-17 13:11:06 -0600

    :sparkles: arreglo en grafhics

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartsService } from "../../shared/services/charts.service"
-import {dataChart} from "../../shared/interfaces/charts.interface"
 
 @Component({
   selector: 'app-home',
@@ -11,15 +10,6 @@ export class HomeComponent implements OnInit {
 
   constructor(private ChartsService:ChartsService) { }
 
-  chart:dataChart={
-    data:[12,32,23,25,31,30,43],
-    labels:["ksjm", "jsn", "jan", "jsj", "hwgw", "jwshw", "jhw"],
-    id:"0",
-    y: true
-  }
-
-  ngOnInit(): void {
-    this.ChartsService.setData(this.chart)
-  }
+  ngOnInit(): void {  }
 
 }
