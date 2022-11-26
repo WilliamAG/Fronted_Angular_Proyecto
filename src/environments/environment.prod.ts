ba579fee6e52ead1799d919dbb7d0e2621aa8131
Luis Ernesto Castillo <71411910+luisescamilla999@users.noreply.github.com>
2022-11-26 11:29:20 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   image: 'https://picspace.onrender.com/',
-  server: 'https://picspace.onrender.com/'
+  server: 'https://picspace.onrender.com/api'
   //image: 'http://localhost:3525/',
   //server: 'http://localhost:3525/api'
 };
